4ead639fde28429fe5f493bc9ed4f6486c43c013
hoop30 <90675192+hoop30@users.noreply.github.com>
2022-01-06 15:51:29 +0100

    Javascript
    finalisation

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-var dropdowns = document.querySelectorAll('#dropdown')
-var txts = document.querySelectorAll('.txt')
-for (var i=0; i<dropdowns.length; i++) {
-    var dropdown = dropdowns[i]
-    
+
+//selectionne tout les dropdowns
+var p = document.querySelectorAll('#dropdown')     
+
+//parcour les dropdowns
+for(var i=0; i<p.length; i++) {
+
+    var ps =p[i]
+     
+    console.log('txt')
     var down = function() {
-        txts.addclasslist = ('disp')
+        var txt = this.children[1]
+        txt.classList.toggle ('disp')
+        var img = this.children[0].children[1]
+        img.classList.toggle ('retourne')
     }
-    dropdown.addEventListener('click',down)
-}
+    ps.addEventListener('click', down)
+}    
